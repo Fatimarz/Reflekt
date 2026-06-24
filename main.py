d6b419090f82667d745358f94dd77a900a34fa6f
Fatima Riaz <std_28927@iobm.edu.pk>
2026-06-24 20:05:29 +0500

    add vercel URL to cors

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.include_router(auth.router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                    "http://127.0.0.1:5173"],
+                    "https://reflekt-lac.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
